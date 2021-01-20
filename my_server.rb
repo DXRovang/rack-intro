0914cfeeaf4753a57d220f64bface3ef74817628
Alexander Rovang <wolf_vii@yahoo.com>
2021-01-20 16:54:30 -0500

    commit tree b8acd7a33fc05a9262dbe9a1811b2ccd562ab33f

--- my_server.rb
+++ my_server.rb
@@ -1,5 +1,4 @@
-class Application
-
+class MyServer
   def call(env)
     return [ 200, {'Content-Type' => 'text/html'}, pretty_response ]
   end
@@ -7,6 +6,4 @@ class Application
   def pretty_response
     (Time.now.to_i % 2).zero? ?  ["<em>Hello</em>"] : ["<strong>Hello</strong>"]
   end
-
-end
-
+end 
